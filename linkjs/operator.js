2ef5991f26274e1309c66684732cf303c599be2e
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2023-09-17 22:21:54 +0700

    versi 1.4
    Operator Matematika

---
+++ linkjs/operator.js
@@ -0,0 +1,55 @@
+let x = 2;
+
+x = -x;
+
+console.log(x);
+
+let p =5, l=6, t=7, volume;
+
+// p = 5;
+// l = 6;
+// t = 7;
+
+volume =  p * l * t;
+
+console.log(volume);
+
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(3/2);
+console.log(7%2);
+
+let city = "sura" + "baya";
+console.log(city);
+
+console.log("1" + 5 + 6);
+console.log(1 + 5 + "6");
+
+console.log(5-"3");
+console.log("5"-3);
+
+console.log("5"/"3");
+console.log(5/"3");
+console.log("5"/"3");
+
+console.log (10 ** 2);
+
+let a , b, c;
+
+a=b=c=4+4;
+
+console.log(c);
+
+let inc = 5;
+ 
+inc++;
+
+console.log(inc);
+
+inc--;
+
+let dec=2;
+dec--;
+
+console.log(dec)

1de10f2fbfef6d786969cb9cc70155f60fa7913d
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2023-09-25 13:07:21 +0700

    versi 1.6
    Percabangan IF

---
+++ linkjs/percabangan.js
@@ -0,0 +1,29 @@
+if(true){
+    console.log("dijalankan jika benar");
+}else{
+    console.log("dijalankan jika salah");
+}
+
+let nilai = 59;
+let kkm = 60;
+let baik = "LULUS";
+let gagal = "TIDAK LULUS";
+let batasatas = 100;
+let batasbawah = 0;
+let warning = "Nilai Salah"
+
+if(nilai <= batasatas && nilai >= batasbawah){
+     if(nilai >= kkm){
+        console.log(baik);
+     }else{
+        console.log(gagal);
+     }
+}else{
+    console.log(warning)
+}
+
+if (nilai >= kkm){
+    console.log(baik); 
+}else{
+    console.log(gagal);
+}

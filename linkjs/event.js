391ac29009954ec2af3928db94df7ec108a579fd
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2023-11-20 11:49:43 +0700

    versi 2.4
    DOM Event

---
+++ linkjs/event.js
@@ -0,0 +1,8 @@
+function show(){
+    a = document.querySelector("p").innerText = "Ini event js.";
+    console.log("Ini event js");
+}
+
+ha2.onclick = function(){
+    document.querySelector(".isi").innerText = "Ini isi";
+}

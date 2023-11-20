87e9305ce237734f76085ffb7bff20673a8ee9bb
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2023-11-20 11:35:12 +0700

    versi 2.3
    Document Object Model

---
+++ linkjs/dom.js
@@ -0,0 +1,7 @@
+document.querySelector("h1").innerText = "HAIIIII.";
+
+document.querySelector("p").innerText = "Deskripsi";
+
+document.querySelector(".isi").innerText = "hah?";
+
+document.querySelector("#ha2").innerText = "haha.";

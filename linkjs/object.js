4d07ae7e641158749bfe68a29bd6d92f13d86237
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2023-11-20 11:13:01 +0700

    versi 2.2
    Object

---
+++ linkjs/object.js
@@ -0,0 +1,10 @@
+const objek = {
+  nama: "Safia Rashida Raya",
+  telp: 081331517890,
+  kucing: ["hitam", "putih", "coklat"],
+  tampil: () => "Nama saya Safia",
+  umur: true,
+};
+
+console.log(objek.tampil());
+console.log(objek.kucing);
